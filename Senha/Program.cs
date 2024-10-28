910819f638e3cecd67f422b8e2c27741b12e4224
Gabriela <gabriela.priaro@gmail.com>
2024-10-28 11:32:11 +0000

    Implementaçao para solicitar senha até que a mesma seja digitada corretamente.

--- Senha/Program.cs
+++ Senha/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Senha
+﻿//Problema "senha_fixa" (adaptado de URI 1114)
+//Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de
+//senha incorreta informada, escrever a mensagem "Senha Invalida! Tente novamente:". Quando a senha
+//for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo
+//encerrado. Considere que a senha correta é o valor 2002. 
+
+namespace Senha
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int senha;
+
+            Console.Write("Digite a senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            { 
+                Console.Write("Senha Invalida! Tente novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido!");
         }
     }
 }
